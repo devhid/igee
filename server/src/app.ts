5f16f528a20035eb7dc57b002025b5f52e849c07
devhid <devhid@users.noreply.github.com>
2020-05-30 01:46:58 -0400

    typo in import path

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import express, { Application } from "express";
-import { Route } from "./models/Route";
+import { Route } from "./models/route";
 import { Middleware } from "./models/middleware";
 
 type ApplicationInputs = {
